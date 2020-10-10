90b6c962f11e345ac62b4f7b43bed711cf119bc5
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2020-10-10 12:33:08 -0700

    dispalying image

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+//function preload() {
+    //img = loadImage('map.jpg');
+//}
+
 function setup() {
     createCanvas(600, 600);
 }
 
 function draw() {
     background(21);
+    //image(img, 0, 0);
+    let img = createImage(66, 66);
+    img.loadPixels();
+    for (let i = 0; i < img.width; i++) {
+      for (let j = 0; j < img.height; j++) {
+        img.set(i, j, color(0, 90, 102, (i % img.width) * 2));
+      }
+    }
+    img.updatePixels();
+    image(img, 17, 17);
+    image(img, 34, 34);
 }
