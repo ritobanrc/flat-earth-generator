f416858a96630fa4b816d6b90f0362de734370dc
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2020-10-10 13:27:09 -0700

    really hacky image display solution

--- sketch.js
+++ sketch.js
@@ -1,22 +1,40 @@
-//function preload() {
-    //img = loadImage('map.jpg');
-//}
+var img = new Image();
+img.crossOrigin = 'anonymous';   //
+img.src = 'map.jpg';
+var canvas = document.getElementById('canvas');
+canvas.style.display = 'none';
+var ctx = canvas.getContext('2d');
+img.onload = function() {
+  ctx.drawImage(img, 0, 0);
+  img.style.display = 'none';
+};
+
+function getColor(x, y) {
+    var pixel = ctx.getImageData(x, y, 1, 1);
+    var data = pixel.data;
+
+    return color(data[0], data[1], data[2])
+}
 
 function setup() {
     createCanvas(600, 600);
+
+    background(21);
 }
 
 function draw() {
     background(21);
-    //image(img, 0, 0);
-    let img = createImage(66, 66);
-    img.loadPixels();
-    for (let i = 0; i < img.width; i++) {
-      for (let j = 0; j < img.height; j++) {
-        img.set(i, j, color(0, 90, 102, (i % img.width) * 2));
+
+    let display = createImage(img.width, img.height);
+    display.loadPixels();
+    for (let i = 0; i < display.width; i++) {
+      for (let j = 0; j < display.height; j++) {
+        //getColor(i, j);
+        display.set(i, j, getColor(i, j));
       }
     }
-    img.updatePixels();
-    image(img, 17, 17);
-    image(img, 34, 34);
+    display.updatePixels();
+    image(display, 17, 17);
+
+    noLoop();
 }
